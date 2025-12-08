a1755c3fb4b3bbd0415e934fe0f431f3c4ba3a2a
rokudara-sen <if23b266@technikum-wien.at>
2025-12-08 17:35:51 +0100

    Controller and Endpoint Added
    Signed-off-by: rokudara-sen <if23b266@technikum-wien.at>

---
+++ MissSolitude.API/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MissSolitude.Infrastructure;
+
+namespace MissSolitude.API;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly DatabaseContext _databaseContext;
+
+    public UserController(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var listOfUsers = _databaseContext.Users.ToList();
+        return Ok(listOfUsers);
+    }
+}
