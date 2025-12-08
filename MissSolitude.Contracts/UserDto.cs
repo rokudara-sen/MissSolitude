757690e07d24d0226579fa9229ce83d6fe32a9d0
rokudara-sen <if23b266@technikum-wien.at>
2025-12-08 18:21:37 +0100

    Added contracts for requests
    Signed-off-by: rokudara-sen <if23b266@technikum-wien.at>

---
+++ MissSolitude.Contracts/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using MissSolitude.Domain;
+
+namespace MissSolitude.Contracts;
+
+public sealed record UserDto(
+    Guid Id,
+    string Username,
+    EmailAddress Email);
+
+public sealed record CreateUserRequest(string Username,
+    string Password,
+    EmailAddress Email);
