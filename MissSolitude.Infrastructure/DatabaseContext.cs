7d672bc702c9a433fd14255690dd8adc8b64a3dd
rokudara-sen <if23b266@technikum-wien.at>
2025-12-08 18:03:07 +0100

    Final touch of EF core basic
    Signed-off-by: rokudara-sen <if23b266@technikum-wien.at>

--- MissSolitude.Infrastructure/DatabaseContext.cs
+++ MissSolitude.Infrastructure/DatabaseContext.cs
@@ -7,8 +7,16 @@ public class DatabaseContext : DbContext
 {
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
     {
-        
     }
-    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(builderUser =>
+        {
+            builderUser.Property(user => user.Email)
+                .HasConversion(email => email.Value, value => new EmailAddress(value)).HasColumnName("Email");
+        });
+    }
+
     public DbSet<User> Users { get; set; }
 }
