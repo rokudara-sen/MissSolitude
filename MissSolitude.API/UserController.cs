cc4a145565442e8418eb03684e1f2014b4a1eb76
rokudara-sen <if23b266@technikum-wien.at>
2025-12-08 21:23:01 +0100

    Removal of uneccesary code
    Signed-off-by: rokudara-sen <if23b266@technikum-wien.at>

--- MissSolitude.API/UserController.cs
+++ MissSolitude.API/UserController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MissSolitude.Application;
 using MissSolitude.Contracts;
-using MissSolitude.Domain;
 using MissSolitude.Infrastructure;
 
 namespace MissSolitude.API;
