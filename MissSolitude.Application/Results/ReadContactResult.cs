043861676c1c24317638b056ec5d2981906b29f4
rokudara-sen <if23b266@technikum-wien.at>
2025-12-15 18:03:04 +0100

    ReadContact Use case added
    Signed-off-by: rokudara-sen <if23b266@technikum-wien.at>

---
+++ MissSolitude.Application/Results/ReadContactResult.cs
@@ -0,0 +1,5 @@
+﻿using MissSolitude.Domain.ValueObjects;
+
+namespace MissSolitude.Application.Results;
+
+public record ReadContactResult(Guid Id, string FirstName, string LastName, EmailAddress? Email = default!, string? Phone = default!, string? Notes = default!);
---
+++ MissSolitude.Application/UseCases/Contact/ReadContactUseCase.cs
@@ -0,0 +1,24 @@
+﻿using MissSolitude.Application.Interfaces.Repositories;
+using MissSolitude.Application.Results;
+
+namespace MissSolitude.Application.UseCases.Contact;
+
+public class ReadContactUseCase
+{
+    private readonly IContactRepository _contactRepository;
+
+    public ReadContactUseCase(IContactRepository contactRepository)
+    {
+        _contactRepository = contactRepository;
+    }
+    
+    public async Task<ReadContactResult> ExecuteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var existingContact = await _contactRepository.GetByIdAsync(id, cancellationToken);
+        
+        if(existingContact is null)
+            throw new KeyNotFoundException("Contact not found.");
+        
+        return new ReadContactResult(existingContact.Id, existingContact.FirstName, existingContact.LastName, existingContact.Email, existingContact.Phone, existingContact.Notes);
+    }
+}
