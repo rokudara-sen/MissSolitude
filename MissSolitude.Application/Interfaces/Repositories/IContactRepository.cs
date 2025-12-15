0ed206b5f2faa1accea39ff3a033e4e380858547
rokudara-sen <if23b266@technikum-wien.at>
2025-12-15 17:31:16 +0100

    Contact repository interface
    Signed-off-by: rokudara-sen <if23b266@technikum-wien.at>

---
+++ MissSolitude.Application/Interfaces/Repositories/IContactRepository.cs
@@ -0,0 +1,14 @@
+﻿using MissSolitude.Domain.Entities;
+
+namespace MissSolitude.Application.Interfaces.Repositories;
+
+public interface IContactRepository
+{
+    Task<bool> FirstAndLastNameExistAsync(string firstName, string lastName, CancellationToken cancellationToken);
+    
+    Task<Contact?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<Contact?> GetByFirstAndLastNameAsync(string firstName, string lastName, CancellationToken cancellationToken);
+    
+    Task AddAsync(Contact contact, CancellationToken cancellationToken);
+    void Remove(Contact contact);
+}
