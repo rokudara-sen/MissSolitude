5233c865dd4ee75f0f534c18bac6e0b44b818967
rokudara-sen <if23b266@technikum-wien.at>
2025-12-16 11:32:55 +0100

    More Unit tests for Contact Controller
    Signed-off-by: rokudara-sen <if23b266@technikum-wien.at>

--- MissSolitude.Tests/Controllers/ContactControllerTest.cs
+++ MissSolitude.Tests/Controllers/ContactControllerTest.cs
@@ -22,7 +22,7 @@ public class ContactControllerTest
         var expectedResult = new CreateContactResult(Guid.NewGuid(), "John", "Doe", new EmailAddress("john@doe.com"), "123", "Note");
         
         useCaseMock
-            .Setup(entity => entity.ExecuteAsync(It.IsAny<CreateContactCommand>(), It.IsAny<CancellationToken>()))
+            .Setup(useCase => useCase.ExecuteAsync(It.IsAny<CreateContactCommand>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(expectedResult);
         
         var controller = new ContactController(useCaseMock.Object, default!, default!, default!);
@@ -46,7 +46,7 @@ public class ContactControllerTest
         var expectedError = "Contact already exists.";
         
         useCaseMock
-            .Setup(entity => entity.ExecuteAsync(It.IsAny<CreateContactCommand>(), It.IsAny<CancellationToken>()))
+            .Setup(useCase => useCase.ExecuteAsync(It.IsAny<CreateContactCommand>(), It.IsAny<CancellationToken>()))
             .ThrowsAsync(new InvalidOperationException(expectedError));
         
         var controller = new ContactController(useCaseMock.Object, default!, default!, default!);
@@ -58,5 +58,100 @@ public class ContactControllerTest
         // Assert
         var badRequestResult = Assert.IsType<BadRequestObjectResult>(response);
         Assert.Equal(expectedError, badRequestResult.Value);
+    }
+
+    [Fact]
+    public async Task Read_shouldReturnOkRequest_WhenUseCaseSucceeds()
+    {
+        // Arrange
+        var useCaseMock = new Mock<ReadContactUseCase>(MockBehavior.Default, default!);
+        var expectedResult = new ReadContactResult(Guid.NewGuid(), "John", "Doe", new EmailAddress("john@doe.com"), "123", "Note");
+        
+        useCaseMock
+            .Setup(useCase => useCase.ExecuteAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedResult);
+
+        var controller = new ContactController(default!, useCaseMock.Object, default!, default!);
+        
+        // Act
+        var response = await controller.ReadContactAsync(expectedResult.Id, CancellationToken.None);
+        
+        // Assert
+        var readResult = Assert.IsType<OkObjectResult>(response);
+        
+        var value = Assert.IsType<ContactDto>(readResult.Value);
+        Assert.Equal(expectedResult.FirstName, value.FirstName);
+    }
+    
+    [Fact]
+    public async Task Read_shouldReturnNotFound_WhenContactDoesNotExist()
+    {
+        // Arrange
+        var useCaseMock = new Mock<ReadContactUseCase>(MockBehavior.Default, default!);
+        var expectedError = "Contact not found.";
+        
+        useCaseMock
+            .Setup(useCase => useCase.ExecuteAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new KeyNotFoundException(expectedError));
+
+        var controller = new ContactController(default!, useCaseMock.Object, default!, default!);
+        
+        // Act
+        var response = await controller.ReadContactAsync(Guid.NewGuid(), CancellationToken.None);
+        
+        // Assert
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(response);
+        Assert.Equal(expectedError, notFoundResult.Value);
+    }
+    
+    [Fact]
+    public async Task Update_shouldReturnOkRequest_WhenUseCaseSucceeds()
+    {
+        // Arrange
+        var useCaseMock = new Mock<UpdateContactUseCase>(MockBehavior.Default, default!, default!);
+        
+        var contactId = Guid.NewGuid(); 
+    
+        var expectedResult = new ReadContactResult(contactId, "John", "Doe", new EmailAddress("john@doe.com"), "123", "Note");
+        var command = new UpdateContactCommand(contactId, "John", "Doe", new EmailAddress("john@doe.com"), "123", "Note");
+
+        useCaseMock
+            .Setup(useCase => useCase.ExecuteAsync(It.IsAny<UpdateContactCommand>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedResult);
+
+        var controller = new ContactController(default!, default!, useCaseMock.Object, default!);
+    
+        // Act
+        var response = await controller.UpdateContactAsync(contactId, command, CancellationToken.None);
+    
+        // Assert
+        var updateResult = Assert.IsType<OkObjectResult>(response);
+        var value = Assert.IsType<ContactDto>(updateResult.Value);
+    
+        Assert.Equal(expectedResult.FirstName, value.FirstName);
+        
+        useCaseMock.Verify(useCase => useCase.ExecuteAsync(command, It.IsAny<CancellationToken>()), Times.Once);
+    }
+    
+    [Fact]
+    public async Task Update_shouldReturnBadRequest_WhenRouteIdDoesNotMatchBodyId()
+    {
+        // Arrange
+        var useCaseMock = new Mock<UpdateContactUseCase>(MockBehavior.Default, default!, default!);
+        var controller = new ContactController(default!, default!, useCaseMock.Object, default!);
+
+        var routeId = Guid.NewGuid();
+        var differentBodyId = Guid.NewGuid();
+    
+        var command = new UpdateContactCommand(differentBodyId, "John", "Doe", new EmailAddress("john@doe.com"), "123", "Note");
+
+        // Act
+        var response = await controller.UpdateContactAsync(routeId, command, CancellationToken.None);
+
+        // Assert
+        var badRequest = Assert.IsType<BadRequestObjectResult>(response);
+        Assert.Equal("Body id and route id do not match.", badRequest.Value);
+        
+        useCaseMock.Verify(x => x.ExecuteAsync(It.IsAny<UpdateContactCommand>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 }
