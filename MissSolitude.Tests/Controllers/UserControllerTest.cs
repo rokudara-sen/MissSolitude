c88967db34fd76f66bbf7d2a7f0b4b01dbb68c83
rokudara-sen <if23b266@technikum-wien.at>
2025-12-16 17:16:56 +0100

    User Read tests added
    Signed-off-by: rokudara-sen <if23b266@technikum-wien.at>

--- MissSolitude.Tests/Controllers/UserControllerTest.cs
+++ MissSolitude.Tests/Controllers/UserControllerTest.cs
@@ -81,4 +81,48 @@ public class UserControllerTest
         var badRequestResult = Assert.IsType<BadRequestObjectResult>(response);
         Assert.Equal(expectedError, badRequestResult.Value);
     }
+
+    [Fact]
+    public async Task Read_shouldReturnOk_WhenUseCaseSucceeds()
+    {
+        // Arrange
+        var useCaseMock = new Mock<ReadUserUseCase>(MockBehavior.Default, default!);
+        var expectedResult = new ReadUserResult(Guid.NewGuid(), "John", new EmailAddress("john@doe.com"));
+        
+        useCaseMock
+            .Setup(useCase => useCase.ExecuteAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedResult);
+
+        var controller = new UserController(default!, useCaseMock.Object, default!, default!, default!);
+        
+        // Act
+        var response = await controller.ReadUserAsync(expectedResult.Id, CancellationToken.None);
+        
+        // Assert
+        var readResult = Assert.IsType<OkObjectResult>(response);
+        
+        var value = Assert.IsType<UserDto>(readResult.Value);
+        Assert.Equal(expectedResult.Email, value.Email);
+    }
+
+    [Fact]
+    public async Task Read_shouldReturnNotFound_WhenUserDoesNotExist()
+    {
+        // Arrange
+        var useCaseMock = new Mock<ReadUserUseCase>(MockBehavior.Default, default!);
+        var expectedError = "User not found.";
+        
+        useCaseMock
+            .Setup(useCase => useCase.ExecuteAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new KeyNotFoundException(expectedError));
+        
+        var controller = new UserController(default!, useCaseMock.Object, default!, default!, default!);
+        
+        // Act
+        var response = await controller.ReadUserAsync(Guid.NewGuid(), CancellationToken.None);
+        
+        // Assert
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(response);
+        Assert.Equal(expectedError, notFoundResult.Value);
+    }
 }
